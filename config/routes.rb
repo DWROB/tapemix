80ef191fb9c424aa927e6be920e74e871749e12c
Dal Robinson <dalwrobinson@protonmail.com>
2022-11-24 19:30:27 +0000

    basic setup working - currently accessing cruel summer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :tracks do
+        collection do
+          get :top_100
+          get :random
+          get :search
+        end
+      end
+    end
+  end
 end
